1c16f067ac6c938230bd2afd0c889b8da05154d6
ZachMuzzle <zach12342@me.com>
2023-01-11 17:02:36 -0500

    Full-stack Webapp

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fullstack App
+
+## How to get Started
+
+* I am following the process of creating a fullstack app with nodejs/express from this video [here](https://www.youtube.com/watch?v=vrj9AohVhPA)
+* I stopped @20:13 minute mark
---
+++ index.js
@@ -0,0 +1,14 @@
+// const { response } = require("express");
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:5000/getAll').then(response => response.json()).then(data => console.log(data));
+    loadHtmlTable([]);
+});
+
+function loadHtmlTable(data) {
+    const table = document.querySelector('table tbody');
+    // let tableHtml = "";
+    if(data.length == 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    }
+}
